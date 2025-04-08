1953ced60491327ebcd2494f0382ca93d907396e
✰cami!! <camila.rosa.candido@escola.pr.gov.br>
2025-04-08 09:51:21 -0300

    s

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;   
 
     });
 
